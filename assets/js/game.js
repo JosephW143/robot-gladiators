9b22619369852cc64d5a181677c33ffd2e7f2e30
JosephW143 <josephwright1436@gmail.com>
2022-06-28 20:07:56 -0400

    add for loop to fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,6 +21,8 @@ for(var i = 0; i < enemyNames.length; i++) {
   }
 
 var fight = function(enemyName) {
+    // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -71,7 +73,10 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     window.alert("You need to choose a valid option. Try again!");
   }
 }
+}
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50
+    fight(pickedEnemyName);
 }
